8637f2bb57f127b898c7d28105e33a32b88b9f5c
sh940701 <visioner2168@gmail.com>
2024-04-19 17:28:45 +0900

    feat: clienterror()

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -20,9 +20,9 @@ void get_filetype(char *filename, char *filetype);
 
 void serve_dynamic(int fd, char *filename, char *cgiargs);
 
-void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
-                 char *longmsg);
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 
+// 실행 arg 로 port number, listen socket fd 입력
 int main(int argc, char **argv) {
     int listenfd, connfd;
     char hostname[MAXLINE], port[MAXLINE];
@@ -44,4 +44,73 @@ int main(int argc, char **argv) {
         doit(connfd);   // line:netp:tiny:doit
         Close(connfd);  // line:netp:tiny:close
     }
+}
+
+
+void doit(int fd) {
+    int is_static;
+    struct stat sbuf;
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char filename[MAXLINE], cgiargs[MAXLINE];
+    rio_t rio;
+
+    // Read request line and headers
+    Rio_readinitb(&rio, fd);
+    Rio_readlineb(&rio, buf, MAXLINE);
+    printf("Request headers:\n");
+    printf("%s", buf);
+    sscanf(buf, "%s %s %s", method, uri, version); // buf 에서 세 개의 문자열을 받아서 method, uri, version 변수에 저장하는 함수
+    if (strcasecmp(method, "GET")) { // 대소문자 구분없이 문자열을 비교하는 함수. 두 값이 같으면 0을 반환하기 때문에 method == get/GET 인 경우 통과한다.
+        clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
+        return;
+    }
+    read_requesthdrs(&rio);
+
+    // 정적 파일 API 인지, 동적 API 인지 파악
+    is_static = parse_uri(uri, filename, cgiargs);
+
+    // filename 에 명시된 파일의 존재여부, 메타데이터 등을 확인하여, valid 한 값이라면 sbuf 에 그 파일 데이터를 채워줌
+    if (stat(filename, &sbuf) < 0) {
+        clienterror(fd, filename, "404", "Not found", "Tiny couldn't find this file");
+        return;
+    }
+
+    // static content handling
+    if (is_static) {
+        // 만약 stat 에 정의된 regular file 이 아니거나, 해당 파일에 대한 읽기 권한이 없다면
+        if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
+            clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
+            return;
+        }
+        serve_static(fd, filename, sbuf.st_size);
+    }
+        // dynamic content handling
+    else {
+        // 만약 stat 에 정의된 regular file 이 아니거나, 해당 파일에 대한 실행 권한이 없다면
+        if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) {
+            clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't run the CGI program");
+            return;
+        }
+        serve_dynamic(fd, filename, cgiargs);
+    }
+}
+
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg) {
+    char buf[MAXLINE], body[MAXBUF];
+
+    /* Build the HTTP response body */
+    sprintf(body, "<html><title>Tiny Error</title>");
+    sprintf(body, "%s<body bgcolor=""ffffff"">", body);
+    sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+    sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+    sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+
+    /* Print the HTTP response */
+    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-type: text/html\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-length: %d\r\n\r\n", (int) strlen(body));
+    Rio_writen(fd, buf, strlen(buf));
+    Rio_writen(fd, body, strlen(body));
 }
