f41c09c2f2a8d214d92663d579e9ad16e719ac98
sh940701 <visioner2168@gmail.com>
2024-04-21 01:01:10 +0900

    feat: proxy server skeleton

--- proxy.c
+++ proxy.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
+#include "./csapp.h"
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
+#define SERVER_PORT 8080
+/* Misc constants */
+#define    MAXLINE     8192  /* Max text line length */
+#define MAXBUF   8192  /* Max I/O buffer size */
+#define LISTENQ  1024  /* Second argument to listen() */
 
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr =
         "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
         "Firefox/10.0.3\r\n";
 
-int main() {
-    printf("%s", user_agent_hdr);
-    return 0;
+void deliver(int fd);
+
+int main(int argc, char **argv) {
+    int listenfd, connfd;
+    char hostname[MAXLINE], port[MAXLINE];
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port\n", argv[0]);
+        exit(1);
+    }
+
+    listenfd = Open_listenfd(argv[1]);
+
+    while (1) {
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *) &clientaddr, &clientlen);
+        Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+        printf("Accepted connection from (%s, %s)\n", hostname, port);
+        deliver(connfd);
+        Close(connfd);
+    }
 }
